c9068f1f04993f48f3318ec7ac9006f8f741dbad
TaiseiShinozaki <shinozakitaisei00@gmail.com>
2020-10-06 18:19:16 +0900

    ユーザー情報登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: true }
+  validates :encrypted_password, presence: true
+  validates :encrypted_password, confirmation: true
+  validates :encrypted_password, length: {minimum: 6 }
+  validates :birth_date, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
 end
