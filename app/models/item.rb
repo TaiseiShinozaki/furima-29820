ed89339dd25208ee14d720f350a384ae791af405
TaiseiShinozaki <shinozakitaisei00@gmail.com>
2020-10-09 17:26:50 +0900

    activehash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :hadling_time
+
+  validates :category, :condition, :postage_payer, :prefecture, :handling_time, presence: true
+
+  validates :category, :condition, :postage_payer, :prefecture, :handling_time, numericality: { other_than: 1 }
 end
